bdebc9a0b429d0b85e7e46cd7d718d2edc26d524
Andrés Castellanos <15945458+andrescv@users.noreply.github.com>
2024-10-17 23:51:29 +0000

    Deploying to main from @ junemoonflorals/site@a43b7edc1c59e194a0d313b567e50167dcc1ab6c 🚀

--- assets/hoisted.B4SdfVHN.js
+++
@@ -1,1 +0,0 @@
-import{L as r}from"./lenis.Bhs7sHwj.js";const i=new r({lerp:.1});function e(n){i.raf(n),requestAnimationFrame(e)}requestAnimationFrame(e);
---
+++ assets/hoisted.CnvxWoy3.js
@@ -0,0 +1,1 @@
+import{L as r}from"./lenis.Bhs7sHwj.js";const i=new r({lerp:.12});function e(n){i.raf(n),requestAnimationFrame(e)}requestAnimationFrame(e);
