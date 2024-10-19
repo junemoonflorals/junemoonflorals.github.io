9721f2a2b0db061ff5e00609d4db4d6d30c75658
Andrés Castellanos <15945458+andrescv@users.noreply.github.com>
2024-10-19 19:26:24 +0000

    Deploying to main from @ junemoonflorals/site@49d6031dd0a45a3f2fec7a055c9994eeb6cd0e46 🚀

--- assets/index.DY16rmGi.js
+++ assets/index.DY16rmGi.js
@@ -1,4 +1,4 @@
-import{r as _a,g as Na}from"./index.DNi1g-pO.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a,g as Na}from"./index.l2PZgWEW.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
